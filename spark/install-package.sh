320f82ce8acc70a1f39ef6a681b5c8a4b443931e
unknown <quang009182@gmail.com>
2023-05-19 12:40:48 +0700

    remove env folder and install_package.sh file

--- spark/install-package.sh
+++
@@ -1,3 +0,0 @@
-wget -P /opt/spark/jars https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.2/hadoop-aws-3.3.2.jar
-wget -P /opt/spark/jars https://repo1.maven.org/maven2/io/delta/delta-core_2.12/2.3.0/delta-core_2.12-2.3.0.jar
-wget -P /opt/spark/jars https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.11.1026/aws-java-sdk-bundle-1.11.1026.jar 
