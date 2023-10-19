e5ee9c4711c6424f81538cf66667dad1d6001772
unknown <quang009182@gmail.com>
2023-10-19 16:09:25 +0700

    add airflow pv, pvc and update deployment file for airflow-webserver-deployment

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import redis
 
 r = redis.Redis(
-    host='100.78.237.90',
+    host='100.73.211.25',
     port=30000,
     password='123456',
     db='0'
 )
 
-r.set('quang', '123')
-print(r.get('quang'))
+r.set('aaaa', '123')
+print(r.get('aaaa'))
